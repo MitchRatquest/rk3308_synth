da26e8c45fbf3e069d1b3b9a63c61b3ac587a2ce
MitchRatQuest <Mitchell.Dokken@gmail.com>
2021-09-14 18:01:23 -0500

    [puredata-iemmatrix] This is an autotools package

--- rk3308/package/puredata-iemmatrix/puredata-iemmatrix.mk
+++ rk3308/package/puredata-iemmatrix/puredata-iemmatrix.mk
@@ -10,23 +10,6 @@ PUREDATA_IEMMATRIX_SITE_METHOD = git
 PUREDATA_IEMMATRIX_AUTORECONF = YES
 PUREDATA_IEMMATRIX_INSTALL_STAGING = YES
 PUREDATA_IEMMATRIX_INSTALL_TARGET = YES
-PUREDATA_IEMMATRIX_DEPENDENCIES = puredata fftw libsndfile
+PUREDATA_IEMMATRIX_DEPENDENCIES = puredata fftw-single fftw-double libsndfile
 
-define PUREDATA_IEMMATRIX_BUILD_CMDS
-	$(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" \
-		PDINCLUDEDIR="$(STAGING_DIR)/usr/include/pd" \
-		PDDIR="$(STAGING_DIR)/usr/lib/pd" \
-		PDBINDIR="$(STAGING_DIR)/usr/lib/pd/bin" \
-		-C $(@D)
-endef
-
-define PUREDATA_IEMMATRIX_INSTALL_TARGET_CMDS
-	$(INSTALL) -d $(TARGET_DIR)/usr/local/lib/pd-externals/iemmatrix
-	find $(@D) -type f -name "*.pd" -print | xargs -I{} $(INSTALL) -m 0755 \
-		{} $(TARGET_DIR)/usr/local/lib/pd-externals/iemmatrix/
-	find $(@D) -type f -name "*.pd_linux" -print | xargs -I{} $(INSTALL) -m 0755 \
-		{} $(TARGET_DIR)/usr/local/lib/pd-externals/iemmatrix/
-	#$(MAKE) install DESTDIR="$(TARGET_DIR)" -C $(@D)
-endef
-
-$(eval $(generic-package))
+$(eval $(autotools-package))
