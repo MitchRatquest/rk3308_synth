8e296f145b44849ccf8041d7cc3c9567991b1e46
MitchRatQuest <Mitchell.Dokken@gmail.com>
2021-09-14 21:14:36 -0500

    [puredata-fftease] typo

--- rk3308/package/puredata-fftease/puredata-fftease.mk
+++ rk3308/package/puredata-fftease/puredata-fftease.mk
@@ -26,7 +26,7 @@ define PUREDATA_FFTEASE_INSTALL_TARGET_CMDS
 		{} $(TARGET_DIR)/usr/local/lib/pd-externals/fftease/
 	find $(@D) -type f -name "*.pd_linux" -print | xargs -I{} $(INSTALL) -m 0755 \
 		{} $(TARGET_DIR)/usr/local/lib/pd-externals/fftease/
-	find $(@D) -type -f -name "*.so" -print | xargs -I{} $(INSTALL) -m 0755 \
+	find $(@D) -type f -name "*.so" -print | xargs -I{} $(INSTALL) -m 0755 \
 		{} $(TARGET_DIR)/usr/local/lib/pd-externals/fftease/
 endef
 
