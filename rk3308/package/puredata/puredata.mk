9785d1fd9721a2f50dd570b060904fde094c98ce
Mitchell Dokken <mitchell.dokken@gmail.com>
2024-01-15 22:57:55 -0600

    [puredata] Add libpd definitions and update the Config file

--- rk3308/package/puredata/puredata.mk
+++ rk3308/package/puredata/puredata.mk
@@ -10,18 +10,35 @@ PUREDATA_SITE_METHOD = git
 PUREDATA_INSTALL_STAGING = YES
 PUREDATA_INSTALL_TARGET = YES
 PUREDATA_AUTORECONF = YES 
-PUREDATA_CONF_OPTS += --disable-portaudio --no-recursion --disable-portmidi
+PUREDATA_CONF_OPTS += --disable-portaudio --no-recursion --disable-portmidi \
+	--with-external-extension=pd_linux --with-deken-cpu=arm64
 PUREDATA_DEPENDENCIES = alsa-utils tcl tk gettext
 
-#ifeq ($(BR2_PACKAGE_JACK2), y)
-#PUREDATA_CONF_OPTS += --enable-jack
-#else
-#PUREDATA_CONF_OPTS += --disable-jack-framework
-#endif
+ifeq ($(BR2_PACKAGE_JACK2), y)
+PUREDATA_CONF_OPTS += --enable-jack
+else
+PUREDATA_CONF_OPTS += --disable-jack-framework
+endif
 
 #ifeq ($(BR2_PACKAGE_FFTW),y)
 #PUREDATA_CONF_OPTS += --enable-fftw
 #endif
+
+ifeq ($(BR2_PACKAGE_LIBPD),y)
+PUREDATA_CONF_OPTS += --enable-libpd
+endif
+
+ifeq ($(BR2_PACKAGE_LIBPD_UTILS),y)
+PUREDATA_CONF_OPTS += --enable-libpd-utils
+endif
+
+ifeq ($(BR2_PACKAGE_LIBPD_INSTANCE),y)
+PUREDATA_CONF_OPTS += --enable-libpd-instance
+endif
+
+ifeq ($(BR2_PACKAGE_LIBPD_EXTRA),y)
+PUREDATA_CONF_OPTS += --enable-libpd-extra
+endif
 
 define PUREDATA_RUN_AUTOGEN
 	cd $(@D) && $(TARGET_MAKE_ENV)  ./autogen.sh
