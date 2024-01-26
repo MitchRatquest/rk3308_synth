91e4e49a535a3406b42cfe04ab2a978f9ff56dd0
Mitchell Dokken <mitchell.dokken@gmail.com>
2024-01-26 17:46:56 -0600

    [puredata-automatonism-reloaded] Add package

---
+++ rk3308/package/puredata-automatonism-reloaded/puredata-automatonism-reloaded.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# puredata-automatonism-reloaded
+#
+################################################################################
+
+PUREDATA_AUTOMATONISM_RELOADED_VERSION = b43445cb82b2f852cde122773fbf32d4cf6f6af4
+PUREDATA_AUTOMATONISM_RELOADED_SITE = https://github.com/jyg/automatonism-reloaded.git
+PUREDATA_AUTOMATONISM_RELOADED_SITE_METHOD = git
+PUREDATA_AUTOMATONISM_RELOADED_INSTALL_STAGING = YES
+PUREDATA_AUTOMATONISM_RELOADED_INSTALL_TARGET = YES
+PUREDATA_AUTOMATONISM_RELOADED_DEPENDENCIES = puredata
+
+define PUREDATA_AUTOMATONISM_RELOADED_BUILD_CMDS
+	true
+endef
+
+define PUREDATA_AUTOMATONISM_RELOADED_INSTALL_TARGET_CMDS
+	$(INSTALL) -d $(TARGET_DIR)/usr/local/lib/pd-externals/automatonism-reloaded
+	cp -a * $(TARGET_DIR)/usr/local/lib/pd-externals/automatonism-reloaded/
+endef
+
+$(eval $(generic-package))
