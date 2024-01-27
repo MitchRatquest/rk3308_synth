df5d2c44f115405e12046f1f829330f094cfb950
Mitchell Dokken <mitchell.dokken@gmail.com>
2024-01-26 23:17:41 -0600

    [orca] Update to latest

--- rk3308/package/orca/orca.mk
+++ rk3308/package/orca/orca.mk
@@ -4,20 +4,29 @@
 #
 ################################################################################
 
-ORCA_VERSION = 50d1c1cc747afc916c43efe4f005754105d81a08
+ORCA_VERSION = e6a9a30ba09f71e18ec85aa613fb0ebdd6c9bec6
 ORCA_SITE = https://github.com/hundredrabbits/Orca-c.git
 ORCA_SITE_METHOD = git
 ORCA_INSTALL_TARGET = YES
 ORCA_DEPENDENCIES = ncurses
 
 define ORCA_BUILD_CMDS
-    $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
+	$(TARGET_MAKE_ENV) $(MAKE) \
+		CC="$(TARGET_CC)" LD="$(TARGET_LD)" \
+		-C $(@D) all
 endef
 
 define ORCA_INSTALL_TARGET_CMDS
     $(INSTALL) -D -m 0755 $(@D)/orca $(TARGET_DIR)/usr/bin
     $(INSTALL) -d -m 0755 $(TARGET_DIR)/usr/share/orca
-    $(INSTALL) -D -m 0644 $(@D)/examples/*.orca $(TARGET_DIR)/usr/share/orca/
+    $(INSTALL) -d -m 0755 $(TARGET_DIR)/usr/share/orca/basics
+    $(INSTALL) -d -m 0755 $(TARGET_DIR)/usr/share/orca/benchmarks
+    $(INSTALL) -d -m 0755 $(TARGET_DIR)/usr/share/orca/misc
+    $(INSTALL) -d -m 0755 $(TARGET_DIR)/usr/share/orca/setups
+    $(INSTALL) -D -m 0644 $(@D)/examples/basics/* $(TARGET_DIR)/usr/share/orca/basics
+    $(INSTALL) -D -m 0644 $(@D)/examples/benchmarks/* $(TARGET_DIR)/usr/share/orca/benchmarks
+    $(INSTALL) -D -m 0644 $(@D)/examples/misc/* $(TARGET_DIR)/usr/share/orca/misc
+    $(INSTALL) -D -m 0644 $(@D)/examples/setups/* $(TARGET_DIR)/usr/share/orca/setups
 endef
 
 $(eval $(generic-package))
