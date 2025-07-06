c272c21bf4165cef7c22a577a2bb4aa3ac683dcb
Mitchell Dokken <mitchell.dokken@gmail.com>
2025-07-06 15:07:35 -0500

    [pocketpy] Add pocketpy, WORK IN PROGRESS, DOES NOT WORK

---
+++ rk3308/package/pocketpy/pocketpy.mk
@@ -0,0 +1,12 @@
+################################################################################
+#
+# pocketpy
+#
+################################################################################
+
+POCKETPY_VERSION = 418f50c29a717d90ed8c2799285eb2d44ea7df02
+POCKETPY_SITE =  https://github.com/pocketpy/pocketpy.git
+POCKETPY_SITE_METHOD = git
+
+$(eval $(cmake-package))
+
