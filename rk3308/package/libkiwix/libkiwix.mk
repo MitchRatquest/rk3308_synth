98ab1a117edc84df78267541c7d2865cbe74470d
Mitchell Dokken <mitchell.dokken@gmail.com>
2025-07-06 16:12:48 -0500

    [libkiwix,libzim] Fix dependencies

--- rk3308/package/libkiwix/libkiwix.mk
+++ rk3308/package/libkiwix/libkiwix.mk
@@ -6,6 +6,6 @@
 LIBKIWIX_VERSION = 14.0.0
 LIBKIWIX_SITE = $(call github,kiwix,libkiwix,$(LIBKIWIX_VERSION))
 LIBKIWIX_INSTALL_STAGING = YES
-LIBKIWIX_DEPENDENCIES = icu pugixml mustache libcurl microhttpd zlib libzim
+LIBKIWIX_DEPENDENCIES = icu pugixml mustache libcurl libmicrohttpd zlib libzim
 
 $(eval $(meson-package))
--- rk3308/package/libzim/libzim.mk
+++ rk3308/package/libzim/libzim.mk
@@ -6,6 +6,6 @@
 LIBZIM_VERSION = 9.3.0
 LIBZIM_SITE = $(call github,openzim,libzim,$(LIBZIM_VERSION))
 LIBZIM_INSTALL_STAGING = YES
-LIBZIM_DEPENDENCIES = lzma icu zstd xapian
+LIBZIM_DEPENDENCIES = icu zstd xapian
 
 $(eval $(meson-package))
