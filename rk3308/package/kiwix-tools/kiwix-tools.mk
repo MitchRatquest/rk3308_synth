910dfdb9d718f897f13a7ebcf69e20f7dfcbb787
Mitchell Dokken <mitchell.dokken@gmail.com>
2025-07-06 16:10:59 -0500

    [kiwix-tools] Declare dependencies in mk

--- rk3308/package/kiwix-tools/kiwix-tools.mk
+++ rk3308/package/kiwix-tools/kiwix-tools.mk
@@ -5,5 +5,6 @@
 ################################################################################
 KIWIX_TOOLS_VERSION = 3.7.0
 KIWIX_TOOLS_SITE = $(call github,kiwix,kiwix-tools,$(KIWIX_TOOLS_VERSION))
+KIWIX_TOOLS_DEPENDENCIES = libzim libkiwix
 
 $(eval $(meson-package))
